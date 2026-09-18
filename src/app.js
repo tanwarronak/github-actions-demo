df9252fc6272a76e54834b44e43f97afa5b02d99
tanwarronak <ronaktanwar73@gmail.com>
2026-09-19 00:01:18 +0530

    github actions demo v1

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
