db3f19e46e4ef957709af980825b66acbe7d3e7a
tanwarronak <ronaktanwar73@gmail.com>
2026-09-20 12:21:10 +0530

    done

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+console.log("we are good to go");
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
